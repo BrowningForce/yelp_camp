8719e8c3e53a2de59d668025565192fcbe79c870
Alexander Penkin <alpenkin3@gmail.com>
2020-05-26 17:46:48 +0300

    added db

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const fetch = require('node-fetch');
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=kharkiv&appid=6d9c019134bbdaea0df1d449dac320cc')
-    .then(res => res.json())
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost/yelp_camp", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// DEFINE SCHEMA
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+// Set model 
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// ROUTERS
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  const campgrounds = Campground.find({}, (err, campgrounds) => {
+    err ? console.log(err) : res.render("campgrounds", { campgrounds });
+  });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/campgrounds", (req, res) => {
+  const { name, image } = req.body;
+
+  Campground.create({
+    name,
+    image,
+  },
+    (err) => (err ? console.log(err) : res.redirect("/campgrounds")));
+});
+
+
+// Server init
+app.listen(process.env.PORT || 3000, () => console.log("Served YelpCamp"));
