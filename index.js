93902ab65bccac3a8152092b3dd88b6d753c2a10
Alexander Penkin <alpenkin3@gmail.com>
2020-06-02 17:36:26 +0300

    Added auth

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const express          = require("express"),
       methodOverride   = require('method-override'),
       expressSanitizer = require('express-sanitizer'),
       mongoose         = require("mongoose"),
+      expressSession   = require('express-session'),
+      passport         = require('passport'),
+      LocalStrategy    = require('passport-local'),
       Campground       = require("./models/campground"),
       Comment          = require('./models/comment'),
+      User             = require('./models/user'),
       seedDB           = require('./seeds');
 
 seedDB();
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
-app.use(express.static('/public'));
+app.use('/assets', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
@@ -20,7 +24,31 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
   useFindAndModify: false,
 });
 
-// ROUTERS
+// PASSPORT CONFIG
+app.use(expressSession({
+  secret: 'Wow, how did you get here?',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//MIDDLEWARE
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// ROUTES
 app.get("/", (req, res) => {
   res.render("landing");
 });
@@ -29,7 +57,7 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   try {
     const campgrounds = await Campground.find({});
-    res.render("index", { campgrounds });
+    res.render("index", { campgrounds, currentUser: req.user });
   } catch (error) {
     console.log(error);
   }
@@ -74,7 +102,7 @@ app.get("/campgrounds/:id", async (req, res) => {
 // ADD COMMENTS ROUTE
 // =====================
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', isLoggedIn, async (req, res) => {
   const { text, author } = req.body.comment;
   try {
     const campground = await Campground.findById(req.params.id);
@@ -89,6 +117,34 @@ app.put('/campgrounds/:id', async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+// Auth route
+app.get('/register', (req, res) => res.render('register'));
+app.post('/register', async (req, res) => {
+  try {
+    await User.register(new User( {username: req.body.username} ), req.body.password);
+    passport.authenticate('local', (req, res, () => {
+      res.redirect('/campgrounds');
+    }));
+  } catch (error) {
+    console.log(error);
+    res.redirect('/register')
+  }
+});
+
+app.get('/secret', isLoggedIn, (req, res) => res.send('this is a secret'));
+
+// Login route
+app.get('/login', (req, res) => res.render('login'));
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/campgrounds',
+  failureRedirect: '/login'
+}), (req, res) => {});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 // Server init
